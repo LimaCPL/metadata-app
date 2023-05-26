3798b5d58ecabc0d8909242c6bab7697a776b47a
lima-cpl <akter-lima@cloudproductionltd.com>
2023-05-26 15:58:56 +0600

    loader added

--- src/components/ReactVideoPlayer.jsx
+++ src/components/ReactVideoPlayer.jsx
@@ -1,46 +1,35 @@
-
-import React,{useState} from 'react';
-import ReactPlayer from 'react-player';
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
 
 const VideoPlayer = () => {
-  const [mediaUrl, setMediaUrl] = useState('');
-  const [isVideo, setIsVideo] = useState(false)
-  const [file, setFile] = useState(null)
-  const [imageMetaData, setImageMetaData] = useState(null)
+  const [mediaUrl, setMediaUrl] = useState("");
+  const [isVideo, setIsVideo] = useState(false);
+  const [file, setFile] = useState(null);
+  const [imageMetaData, setImageMetaData] = useState(null);
   const [videoMetadata, setVideoMetadata] = React.useState(null);
-  const [dataLoaded, setDataLoaded] = useState(false)
-  const [videoResolution, setVideoResolution] = useState('');
-//   const [loader, setLoader] = useState(false);
-//   const [fileType, setFileType] = useState("");
-
-const execImage = (THIS) => {
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [videoResolution, setVideoResolution] = useState("");
+  const [loading, setLoading] = useState(false);
+  const execImage = (THIS) => {
     const resolution = gcd(THIS.width, THIS.height);
-    console.log(THIS)
+    console.log(THIS);
     const fileData = [
       {
         name: "形式",
         value: "Image",
-      }
+      },
     ];
 
-  setImageMetaData({
-    type: 'image',
-    aspectRatio: `${THIS.width / resolution + ":" + THIS.height / resolution}`,
-    resolution: `${THIS.width + "X" + THIS.height}`
-  })
-    // fileData.push({
-    //   name: "アスペクト比",
-    //   value: THIS.width / resulotion + ":" + THIS.height / resulotion,
-    // });
-    // fileData.push({
-    //   name: "解像度",
-    //   value: THIS.width + "X" + THIS.height,
-    // });
-    
-   // setLoader(false);
+    setImageMetaData({
+      type: "image",
+      aspectRatio: `${
+        THIS.width / resolution + ":" + THIS.height / resolution
+      }`,
+      resolution: `${THIS.width + "X" + THIS.height}`,
+    });
   };
 
-const getWidthFromFile = (file, callback) => {
+  const getWidthFromFile = (file, callback) => {
     const reader = new FileReader();
 
     reader.onload = function (event) {
@@ -48,34 +37,43 @@ const getWidthFromFile = (file, callback) => {
       const image = new Image();
       image.onload = function () {
         callback(this);
+        setLoading(false);
       };
       image.src = dataUrl;
     };
     reader.readAsDataURL(file);
   };
 
-
   const gcd = (a, b) => {
     return b == 0 ? a : gcd(b, a % b);
   };
   const handleFileUpload = (e) => {
+    setLoading(true);
 
     const selectedFile = e.target.files[0];
     const fileUrl = URL.createObjectURL(selectedFile);
-    setFile(e.target.files[0])
-    // setFile({
-    //     file: e.target.files[0],
-    // })
-    console.log(e.target.files[0])
-    if(e.target.files[0].type.includes('video')) {
-        setIsVideo(true)
-    }else {
-        getWidthFromFile(e.target.files[0], execImage);
+    setFile(e.target.files[0]);
 
-        setIsVideo(false);
+    console.log(e.target.files[0]);
+
+    if (e.target.files[0].type.includes("video")) {
+      
+      setIsVideo(true);
+    } else {
+      getWidthFromFile(e.target.files[0], execImage);
+
+      setIsVideo(false);
     }
-    setDataLoaded(true)
+
+    // setDataLoaded(true);
+    // setLoading(false);
+    // setMediaUrl(fileUrl);
+
+    setTimeout(() => {
+    setDataLoaded(true);
     setMediaUrl(fileUrl);
+    setLoading(false);
+    }, 2000);
   };
 
   const handleVideoDuration = (duration) => {
@@ -85,144 +83,159 @@ const getWidthFromFile = (file, callback) => {
     }));
   };
 
-
   const handleVideoReady = () => {
-    const player = document.querySelector('video');
+
+    const player = document.querySelector("video");
     const resolution = `${player.videoWidth}x${player.videoHeight}`;
     setVideoResolution(resolution);
-   
-    const hasSound = player.mozHasAudio || Boolean(player.webkitAudioDecodedByteCount) || Boolean(player.audioTracks?.length);
+
+    const hasSound =
+      player.mozHasAudio ||
+      Boolean(player.webkitAudioDecodedByteCount) ||
+      Boolean(player.audioTracks?.length);
     const { videoWidth, videoHeight } = player;
     const aspectRatio = videoWidth / videoHeight;
+    console.log('hello hello')
+ 
     setVideoMetadata((prevMetadata) => ({
       ...prevMetadata,
       hasSound,
       resolution,
       aspectRatio: aspectRatio.toFixed(2),
-      
     }));
+  
   };
 
   React.useEffect(() => {
     if (mediaUrl) {
-      const videoType = mediaUrl.split('.').pop();
-      setVideoMetadata({ type: 'video' });
+      const videoType = mediaUrl.split(".").pop();
+      setVideoMetadata({ type: "video" });
     }
   }, [mediaUrl]);
 
   return (
-
-    <div className='file-upload file-upload-wrapper'>
-      {/* <input type="file" onChange={handleFileUpload} /> */}
-      {videoMetadata && isVideo && dataLoaded && (
-
-<div className="metaData-table-wrapper">
-        <h4 className="file-title">
-    <span>
-      <img src="movie-icon.png" />
-    </span>
-    焼き芋できたて
-  </h4>
-        <table>
-          <thead>
-            <tr>
-              <th>Metadata</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-         
-            <tr>
-              <td>形式</td>
-              <td>{videoMetadata.type}</td>
-            </tr>
-            <tr>
-              <td>オーディオ</td>
-              <td>{videoMetadata.hasSound ? 'あり' : 'いいえ'}</td>
-            </tr>
-            <tr>
-            <td>アスペクト比</td>
-             <td>{videoMetadata.aspectRatio}</td>
-           </tr>
-           <tr>
-              <td>再生時間</td>
-              <td>{videoMetadata.duration} 秒</td>
-            </tr>
-            <tr>
-              <td>解像度</td>
-              <td>{videoMetadata.resolution}</td>
-            </tr>
-         
-          </tbody>
-        </table>
+    <div className="file-upload file-upload-wrapper">
+      {!loading && videoMetadata && isVideo && dataLoaded && (
+        <div className="metaData-table-wrapper">
+          <h4 className="file-title">
+            <span>
+              <img src="movie-icon.png" alt="icon" />
+            </span>
+            焼き芋できたて
+          </h4>
+          <table>
+            <thead>
+              <tr>
+                <th>Metadata</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>形式</td>
+                <td>{videoMetadata.type}</td>
+              </tr>
+              <tr>
+                <td>オーディオ</td>
+                <td>{videoMetadata.hasSound ? "あり" : "いいえ"}</td>
+              </tr>
+              <tr>
+                <td>アスペクト比</td>
+                <td>{videoMetadata.aspectRatio}</td>
+              </tr>
+              <tr>
+                <td>再生時間</td>
+                <td>{videoMetadata.duration} 秒</td>
+              </tr>
+              <tr>
+                <td>解像度</td>
+                <td>{videoMetadata.resolution}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-     
       )}
-        {!isVideo && dataLoaded && (
-          <>
-              <div className="metaData-table-wrapper">
-        <h4 className="file-title">
-    <span>
-      <img src="movie-icon.png" />
-    </span>
-    焼き芋できたて
-  </h4>
-        <table>
-          <thead>
-            <tr>
-              <th>Metadata</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-         
-            <tr>
-              <td>形式</td>
-              <td>{imageMetaData?.type}</td>
-            </tr>
-           
-            <tr>
-            <td>アスペクト比</td>
-             <td>{imageMetaData?.aspectRatio}</td>
-           </tr>
-           <tr>
-            <td>解像度</td>
-             <td>{imageMetaData?.resolution}</td>
-           </tr>
-          </tbody>
-        </table>
-        </div>
+      {!loading && !isVideo && dataLoaded && (
+        <>
+          <div className="metaData-table-wrapper">
+            <h4 className="file-title">
+              <span>
+                <img src="movie-icon.png" />
+              </span>
+              焼き芋できたて
+            </h4>
+            <table>
+              <thead>
+                <tr>
+                  <th>Metadata</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>形式</td>
+                  <td>{imageMetaData?.type}</td>
+                </tr>
+
+                <tr>
+                  <td>アスペクト比</td>
+                  <td>{imageMetaData?.aspectRatio}</td>
+                </tr>
+                <tr>
+                  <td>解像度</td>
+                  <td>{imageMetaData?.resolution}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </>
       )}
-    <div className="file-input-preview">
-  
-              <div className="file-preview">
-                {isVideo && <ReactPlayer url={mediaUrl}   className="previewElement"   width="400" controls onDuration={handleVideoDuration} onReady={handleVideoReady} />}
-               
-                {!isVideo && <img className="previewElement" src={mediaUrl} alt="Preview" /> }
-              
-              </div>
+
+      <div className="file-input-preview">
+        {/* {loading &&   <div className="loader">loading</div>} */}
+
+        <div className="file-preview">
+
+        {loading ? (
+        <div className="loader">Loading...</div>
+      ) : (
+        <>
+        {!loading && isVideo &&  (
+          <ReactPlayer
+          playing
+            url={mediaUrl}
+            className="previewElement"
+            width="400"
+            controls
+            onDuration={handleVideoDuration}
+            onReady={handleVideoReady}
+            autoPlay={true}
+          />
+        )}
+
+        {!loading && !isVideo && (
+          <img className="previewElement" src={mediaUrl} alt="Preview" />
+        )}
+        </>
+      )}
+
          
-          </div>
-          <div className="upload-button-wrapper">
-            <label htmlFor="file-input" className="file-label">
-            アップロード
-            </label>
-            <input type="file"  id="file-input" onChange={handleFileUpload} className="file-upload" />
-          </div>
+        </div>
+      </div>
 
-
-
-
-      
-   
-   
-   
-   
+      <div className="upload-button-wrapper">
+        <label htmlFor="file-input" className="file-label">
+          アップロード
+        </label>
+        <input
+          type="file"
+          id="file-input"
+          onChange={handleFileUpload}
+          className="file-upload"
+        />
+      </div>
     </div>
   );
 };
 
 export default VideoPlayer;
-
-
